9bd2dd9557cdfb1cdec7ec99e06c7f1f8de228e5
garikek <yan.buinov@gmail.com>
2024-11-04 13:51:41 +0300

    feat: init eureka discovery server

---
+++ eureka-server/src/test/java/com/software/modsen/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.modsen.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
