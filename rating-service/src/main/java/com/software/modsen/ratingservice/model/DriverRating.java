01754018698aed6b03d5aa27962c2487880bd94f
garikek <yan.buinov@gmail.com>
2024-11-14 17:44:10 +0300

    refactor: move common entity fields to abstract class

--- rating-service/src/main/java/com/software/modsen/ratingservice/model/DriverRating.java
+++ rating-service/src/main/java/com/software/modsen/ratingservice/model/DriverRating.java
@@ -2,11 +2,6 @@ package com.software.modsen.ratingservice.model;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -15,8 +10,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 @Table(name = "driver_rating")
-@EntityListeners(AuditingEntityListener.class)
-public class DriverRating {
+public class DriverRating extends BaseRating {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -25,12 +19,4 @@ public class DriverRating {
     private Long driverId;
 
     private Double rating = 0.0;
-
-    @CreatedDate
-    @Column(name = "created_at", updatable = false)
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    @Column(name = "updated_at")
-    private LocalDateTime updatedAt;
 }
--- rating-service/src/main/java/com/software/modsen/ratingservice/model/PassengerRating.java
+++ rating-service/src/main/java/com/software/modsen/ratingservice/model/PassengerRating.java
@@ -2,11 +2,6 @@ package com.software.modsen.ratingservice.model;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -15,8 +10,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 @Table(name = "passenger_rating")
-@EntityListeners(AuditingEntityListener.class)
-public class PassengerRating {
+public class PassengerRating extends BaseRating {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -25,12 +19,4 @@ public class PassengerRating {
     private Long passengerId;
 
     private Double rating = 0.0;
-
-    @CreatedDate
-    @Column(name = "created_at", updatable = false)
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    @Column(name = "updated_at")
-    private LocalDateTime updatedAt;
 }
