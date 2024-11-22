a0f66c5a8ee236eab1626c9bf880d653d8b7e5f8
garikek <yan.buinov@gmail.com>
2024-11-22 18:10:39 +0300

    feat (ride-service): add controller layer

---
+++ ride-service/src/main/java/com/software/modsen/rideservice/controller/impl/RideController.java
@@ -0,0 +1,52 @@
+package com.software.modsen.rideservice.controller.impl;
+
+import com.software.modsen.rideservice.controller.RideApi;
+import com.software.modsen.rideservice.dto.RideListDto;
+import com.software.modsen.rideservice.dto.RideRequest;
+import com.software.modsen.rideservice.dto.RideResponse;
+import com.software.modsen.rideservice.service.RideService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/rides")
+public class RideController implements RideApi {
+    private final RideService rideService;
+
+    @Override
+    @GetMapping
+    public ResponseEntity<RideListDto> getRides() {
+        return ResponseEntity.ok(rideService.getRides());
+    }
+
+    @Override
+    @PostMapping
+    public ResponseEntity<RideResponse> addRide(@RequestBody RideRequest rideRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(rideService.addRide(rideRequest));
+    }
+
+    @Override
+    @GetMapping("/{id}")
+    public RideResponse getRideById(@PathVariable Long id) {
+        return rideService.getRideById(id);
+    }
+
+    @Override
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void>  deleteRide(@PathVariable Long id) {
+        rideService.deleteRide(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @Override
+    @PutMapping("/{id}")
+    public ResponseEntity<RideResponse> updateRide(
+            @PathVariable Long id,
+            @RequestBody RideRequest rideRequest
+    ) {
+        return ResponseEntity.ok(rideService.updateRide(id, rideRequest));
+    }
+}
