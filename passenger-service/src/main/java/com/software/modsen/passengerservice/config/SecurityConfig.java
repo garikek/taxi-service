a35aeee70fbcf877a929425c94fb2328f4112aa1
garikek <yan.buinov@gmail.com>
2025-01-06 18:23:30 +0300

    feat: add security configuration

---
+++ passenger-service/src/main/java/com/software/modsen/passengerservice/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.software.modsen.passengerservice.config;
+
+import com.software.modsen.passengerservice.security.JwtAuthConverter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtAuthConverter jwtAuthConverter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .oauth2ResourceServer(oauth
+                        -> oauth.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                )
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers(HttpMethod.GET, "/actuator/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                );
+        return http.build();
+    }
+
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(jwtAuthConverter);
+        return converter;
+    }
+}
