cda4882fae95f0385c703802e6f192c7a6f9698d
garikek <yan.buinov@gmail.com>
2025-01-10 23:36:31 +0300

    feat: add authentication swagger pattern

--- driver-service/src/main/java/com/software/modsen/driverservice/config/SwaggerConfig.java
+++ driver-service/src/main/java/com/software/modsen/driverservice/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.software.modsen.driverservice.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,6 +23,15 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .info(new Info().title("Driver Service API")
                         .version("1.0")
-                        .description("API documentation for Driver Service"));
+                        .description("API documentation for Driver Service"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("Bearer Authentication",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("Authorization")));
     }
 }
